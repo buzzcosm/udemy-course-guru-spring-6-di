4f30d2e0aead954cfe56fdd87033dc8d72cc19a2
Jeff_Buzz <97390468+buzzcosm@users.noreply.github.com>
2025-04-19 20:34:22 +0200

    Call controller method with application-context and directly in test-class

--- src/test/java/com/buzzcosm/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/com/buzzcosm/spring6di/Spring6DiApplicationTests.java
@@ -1,10 +1,30 @@
 package com.buzzcosm.spring6di;
 
+import com.buzzcosm.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
+
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+
+	@Test
+	void testAutowireOfController() {
+		System.out.println(myController.sayHello());
+	}
+
+	@Test
+	void testGetControllerFromApplicationContext() {
+		MyController controller = applicationContext.getBean(MyController.class);
+		System.out.println(controller.sayHello());
+	}
 
 	@Test
 	void contextLoads() {
