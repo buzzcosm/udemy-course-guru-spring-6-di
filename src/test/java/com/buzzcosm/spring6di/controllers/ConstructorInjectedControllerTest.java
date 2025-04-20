b92d65ca77680d693f4154ada2998d9412afea0b
Jeff_Buzz <97390468+buzzcosm@users.noreply.github.com>
2025-04-20 12:03:32 +0200

    Cleanup

--- src/test/java/com/buzzcosm/spring6di/controllers/ConstructorInjectedControllerTest.java
+++ src/test/java/com/buzzcosm/spring6di/controllers/ConstructorInjectedControllerTest.java
@@ -1,12 +1,8 @@
 package com.buzzcosm.spring6di.controllers;
 
-import com.buzzcosm.spring6di.services.GreetingServiceImpl;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ConstructorInjectedControllerTest {
--- src/test/java/com/buzzcosm/spring6di/controllers/i18n/Myi18NControllerEnglishTest.java
+++ src/test/java/com/buzzcosm/spring6di/controllers/i18n/Myi18NControllerEnglishTest.java
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @ActiveProfiles("EN")
 @SpringBootTest
 class Myi18NControllerEnglishTest {
